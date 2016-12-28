2b571a81cef05a16635cccf6928ebafd056a320c
Philippe M. Chiasson <gozer@ectoplasm.org>
2016-12-28 13:08:05 -0500

    puppet-3 friendly

--- manifests/update.pp
+++ manifests/update.pp
@@ -8,10 +8,16 @@ class nubis_apache::update($script_source, $script_interval=undef) {
   }
 
   if $script_interval {
+    validate_hash($script_interval)
+
     cron::job { 'update-site':
       command => 'nubis-cron update-site /usr/local/bin/nubis-update-site',
       user    => 'root',
-      *       => $script_interval,
+      minute  => pick($script_interval['minute'], '*'),
+      hour    => pick($script_interval['hour'], '*'),
+      date    => pick($script_interval['date'], '*'),
+      month   => pick($script_interval['month'], '*'),
+      weekday => pick($script_interval['weekday'], '*'),
     }
   }
 }
