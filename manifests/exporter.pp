f9fe3e052bc64b81260fe2c752431ad7df23ef01
Philippe M. Chiasson <gozer@ectoplasm.org>
2017-10-23 12:56:03 -0400

    make systemd friendly

--- manifests/exporter.pp
+++ manifests/exporter.pp
@@ -18,38 +18,62 @@ file { '/usr/local/bin/apache_exporter':
 case $::osfamily {
   'RedHat': {
     $apache_exporter_default = '/etc/sysconfig/apache_exporter'
-    file { '/etc/init.d/apache_exporter':
-      ensure => file,
-      owner  => root,
-      group  => root,
-      mode   => '0755',
-      source => "puppet:///modules/${module_name}/apache_exporter.init",
-    }->
-    service { 'apache_exporter':
-      enable => true,
-    }
   }
   'Debian': {
     $apache_exporter_default = '/etc/default/apache_exporter'
-    file { '/etc/init/apache_exporter.conf':
-      ensure => file,
-      owner  => root,
-      group  => root,
-      mode   => '0644',
-      source => "puppet:///modules/${module_name}/apache_exporter.upstart",
-    }
   }
   default: {
     fail("Unsupported OS for apache_exporter ${::osfamily}")
   }
 }
 
+if ($::systemd) {
+  file { '/lib/systemd/system/apache_exporter.service':
+    ensure => file,
+    owner  => root,
+    group  => root,
+    mode   => '0755',
+    source => "puppet:///modules/${module_name}/apache_exporter.systemd",
+  }->
+  service { 'apache_exporter':
+    enable => true,
+  }
+}
+else {
+  case $::osfamily {
+    'RedHat': {
+      file { '/etc/init.d/apache_exporter':
+        ensure => file,
+        owner  => root,
+        group  => root,
+        mode   => '0755',
+        source => "puppet:///modules/${module_name}/apache_exporter.init",
+      }->
+      service { 'apache_exporter':
+        enable => true,
+      }
+    }
+    'Debian': {
+      file { '/etc/init/apache_exporter.conf':
+        ensure => file,
+        owner  => root,
+        group  => root,
+        mode   => '0644',
+        source => "puppet:///modules/${module_name}/apache_exporter.upstart",
+      }
+    }
+    default: {
+      fail("Unsupported OS for apache_exporter ${::osfamily}")
+    }
+  }
+}
+
 file { $apache_exporter_default:
-  ensure => file,
-  owner  => root,
-  group  => root,
-  mode   => '0644',
-  content => "SCRAPE_URI=http://localhost:$port/server-status/?auto"
+  ensure  => file,
+  owner   => root,
+  group   => root,
+  mode    => '0644',
+  content => "SCRAPE_URI=http://localhost:${port}/server-status/?auto"
 }
 
 file { '/etc/consul/svc-apache-exporter.json':
